41a03a399cc69e3f63f2b0483369f2d4c7623a85
verenyerikodirgantara <yerikodirgantara@gmail.com>
2024-10-05 12:40:16 +0700

    reguser update

--- src/components/Register/Reguser.vue
+++ src/components/Register/Reguser.vue
@@ -1,156 +1,180 @@
 <template>
-    <div class="page-container">
-      <div class="register-container">
-        <h1 class="register-title">Register</h1>
-        <form @submit.prevent="registerUser">
-          <!-- Username -->
-          <div class="form-group" :class="{ 'has-error': usernameError }">
-            <label for="username">NIK (Nomor Induk Kependudukan) Murid:</label>
+  <div class="page-container">
+    <div class="register-container">
+      <h1 class="register-title">Register</h1>
+      <form @submit.prevent="registerUser">
+        <!-- NIK -->
+        <div class="form-group" :class="{ 'has-error': nikError }">
+          <label for="nik">NIK (Nomor Induk Kependudukan) Murid:</label>
+          <input
+            type="text"
+            id="nik"
+            v-model="user.nik"
+            @blur="validateNik"
+            @input="clearError('nikError')"
+            required
+            class="input-field"
+          />
+          <p v-if="nikError" class="error">{{ nikError }}</p>
+        </div>
+
+        <!-- Username -->
+        <div class="form-group" :class="{ 'has-error': usernameError }">
+        <label for="username">Nama Murid:</label>
+        <input
+          type="text"
+          id="username"
+          v-model="user.username"
+          @blur="validateUsername"
+          @input="clearError('usernameError')"
+          required
+          class="input-field"
+        />
+        <p v-if="usernameError" class="error">{{ usernameError }}</p>
+      </div>
+
+
+        <!-- Password -->
+        <div class="form-group" :class="{ 'has-error': passwordError }">
+          <label for="password">Kata Sandi:</label>
+          <div class="password-wrapper">
             <input
-              type="text"
-              id="username"
-              v-model="user.username"
-              @blur="validateUsername"
-              @input="clearError('usernameError')"
+              :type="showPassword ? 'text' : 'password'"
+              id="password"
+              v-model="user.password"
+              @blur="validatePassword"
+              @input="clearError('passwordError')"
               required
               class="input-field"
             />
-            <p v-if="usernameError" class="error">{{ usernameError }}</p>
+            <button type="button" @click="togglePasswordVisibility" class="toggle-password">
+              <i :class="showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'"></i>
+            </button>
           </div>
-  
-          <!-- Password -->
-          <div class="form-group" :class="{ 'has-error': passwordError }">
-            <label for="password">Kata Sandi:</label>
-            <div class="password-wrapper">
-              <input
-                :type="showPassword ? 'text' : 'password'"
-                id="password"
-                v-model="user.password"
-                @blur="validatePassword"
-                @input="clearError('passwordError')"
-                required
-                class="input-field"
-              />
-              <button type="button" @click="togglePasswordVisibility" class="toggle-password">
-                <i :class="showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'"></i>
-              </button>
-            </div>
-            <p v-if="passwordError" class="error">{{ passwordError }}</p>
+          <p v-if="passwordError" class="error">{{ passwordError }}</p>
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="form-group" :class="{ 'has-error': confirmPasswordError }">
+          <label for="confirm-password">Konfirmasi Kata Sandi:</label>
+          <div class="password-wrapper">
+            <input
+              :type="showConfirmPassword ? 'text' : 'password'"
+              id="confirm-password"
+              v-model="user.confirmPassword"
+              @blur="validateConfirmPassword"
+              @input="clearError('confirmPasswordError')"
+              required
+              class="input-field"
+            />
+            <button type="button" @click="toggleConfirmPasswordVisibility" class="toggle-password">
+              <i :class="showConfirmPassword ? 'fas fa-eye-slash' : 'fas fa-eye'"></i>
+            </button>
           </div>
-  
-          <!-- Confirm Password -->
-          <div class="form-group" :class="{ 'has-error': confirmPasswordError }">
-            <label for="confirm-password">Konfirmasi Kata Sandi:</label>
-            <div class="password-wrapper">
-              <input
-                :type="showConfirmPassword ? 'text' : 'password'"
-                id="confirm-password"
-                v-model="user.confirmPassword"
-                @blur="validateConfirmPassword"
-                @input="clearError('confirmPasswordError')"
-                required
-                class="input-field"
-              />
-              <button type="button" @click="toggleConfirmPasswordVisibility" class="toggle-password">
-                <i :class="showConfirmPassword ? 'fas fa-eye-slash' : 'fas fa-eye'"></i>
-              </button>
-            </div>
-            <p v-if="confirmPasswordError" class="error">{{ confirmPasswordError }}</p>
-          </div>
-  
-          <!-- Submit Button -->
-          <button type="submit" class="register-button">Register</button>
-  
-          <!-- Error and Success Messages -->
-          <p v-if="errorMessage" class="error message">{{ errorMessage }}</p>
-          <p v-if="successMessage" class="success message">
-            {{ successMessage }}
-          </p>
-  
-          <!-- Link to Login -->
-          <div class="login-link-container">
-            <span>Sudah mendaftar? Silahkan Login </span>
-            <router-link to="/Loguser" class="login-link"> disini</router-link>
-          </div>
-        </form>
-      </div>
+          <p v-if="confirmPasswordError" class="error">{{ confirmPasswordError }}</p>
+        </div>
+
+        <!-- Submit Button -->
+        <button type="submit" class="register-button">Register</button>
+
+        <!-- Error and Success Messages -->
+        <p v-if="errorMessage" class="error message">{{ errorMessage }}</p>
+        <p v-if="successMessage" class="success message">{{ successMessage }}</p>
+
+        <!-- Link to Login -->
+        <div class="login-link-container">
+          <span>Sudah mendaftar? Silahkan Login </span>
+          <router-link to="/Loguser" class="login-link"> disini</router-link>
+        </div>
+      </form>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  import Swal from 'sweetalert2';
-  
-  export default {
-    data() {
-      return {
-        user: {
-          username: '',
-          password: '',
-          confirmPassword: '',
-        },
-        usernameError: '',
-        passwordError: '',
-        confirmPasswordError: '',
-        errorMessage: '',
-        successMessage: '',
-        showPassword: false,
-        showConfirmPassword: false,
-      };
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
+export default {
+  data() {
+    return {
+      user: {
+        nik: '',
+        username: '',  // Mengganti 'name' dengan 'username'
+        password: '',
+        confirmPassword: '',
+      },
+      nikError: '',
+      usernameError: '',  // Mengganti 'nameError' dengan 'usernameError'
+      passwordError: '',
+      confirmPasswordError: '',
+      errorMessage: '',
+      successMessage: '',
+      showPassword: false,
+      showConfirmPassword: false,
+    };
+  },
+  methods: {
+    clearError(field) {
+      this[field] = '';
     },
-    methods: {
-      clearError(field) {
-        this[field] = '';
-      },
-  
-      validateUsername() {
-        if (this.user.username.length < 3) {
-          this.usernameError = 'NIK harus 16 karakter.';
-          Swal.fire('Error', this.usernameError, 'error');
-        }
-      },
-  
-      validatePassword() {
-        if (this.user.password.length < 4) { // Adjusted password length to 4 characters
-          this.passwordError = 'Kata sandi harus minimal 4 karakter.';
-          Swal.fire('Error', this.passwordError, 'error');
-        }
-      },
-  
-      validateConfirmPassword() {
-        if (this.user.password !== this.user.confirmPassword) {
-          this.confirmPasswordError = 'Kata sandi tidak cocok.';
-          Swal.fire('Error', this.confirmPasswordError, 'error');
-        }
-      },
-  
-      async registerUser() {
-        if (this.usernameError || this.passwordError || this.confirmPasswordError) {
-          return;
-        }
-  
-        try {
-          const response = await axios.post('https://api.example.com/register', this.user);
-          this.successMessage = 'Registration successful! Please log in.';
-          this.errorMessage = '';
-          Swal.fire('Success', 'Registration successful!', 'success');
-        } catch (error) {
-          this.errorMessage = 'Registration failed. Please try again.';
-          this.successMessage = '';
-          Swal.fire('Error', this.errorMessage, 'error');
-        }
-      },
-  
-      togglePasswordVisibility() {
-        this.showPassword = !this.showPassword;
-      },
-  
-      toggleConfirmPasswordVisibility() {
-        this.showConfirmPassword = !this.showConfirmPassword;
-      },
+
+    validateNik() {
+      if (this.user.nik.length !== 16) {
+        this.nikError = 'NIK harus 16 karakter.';
+        Swal.fire('Error', this.nikError, 'error');
+      }
     },
-  };
-  </script>
+
+    validateUsername() {  // Mengganti 'validateName' menjadi 'validateUsername'
+      if (this.user.username.length < 3) {
+        this.usernameError = 'Nama harus minimal 3 karakter.';
+        Swal.fire('Error', this.usernameError, 'error');
+      }
+    },
+
+    validatePassword() {
+      if (this.user.password.length < 4) {
+        this.passwordError = 'Kata sandi harus minimal 4 karakter.';
+        Swal.fire('Error', this.passwordError, 'error');
+      }
+    },
+
+    validateConfirmPassword() {
+      if (this.user.password !== this.user.confirmPassword) {
+        this.confirmPasswordError = 'Kata sandi tidak cocok.';
+        Swal.fire('Error', this.confirmPasswordError, 'error');
+      }
+    },
+
+    async registerUser() {
+      if (this.nikError || this.usernameError || this.passwordError || this.confirmPasswordError) {
+        return;
+      }
+
+      try {
+        const response = await axios.post('https://api.example.com/register', this.user);
+        this.successMessage = 'Registration successful! Please log in.';
+        this.errorMessage = '';
+        Swal.fire('Success', 'Registration successful!', 'success');
+      } catch (error) {
+        this.errorMessage = 'Registration failed. Please try again.';
+        this.successMessage = '';
+        Swal.fire('Error', this.errorMessage, 'error');
+      }
+    },
+
+    togglePasswordVisibility() {
+      this.showPassword = !this.showPassword;
+    },
+
+    toggleConfirmPasswordVisibility() {
+      this.showConfirmPassword = !this.showConfirmPassword;
+    },
+  },
+};
+</script>
+
   
   <style scoped>
   .page-container {
